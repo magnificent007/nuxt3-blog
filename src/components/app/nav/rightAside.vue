4eef6ecc449244a5ce844fe3a6ec86fc5120efd4
xinlin <1451985060@qq.com>
2024-11-30 23:14:24 +0800

    新增文章special one

--- src/components/app/nav/rightAside.vue
+++ src/components/app/nav/rightAside.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="p-4 flex-col gap-4 hidden lg:flex">
+  <div class="p-4 flex-col gap-4 hidden lg:flex bg-zinc-50 dark:bg-slate-900">
     <AppNavSeason />
   </div>
 </template>
--- src/components/app/nav/season.vue
+++ src/components/app/nav/season.vue
@@ -24,10 +24,15 @@ const season = computed(() => {
 
 <template>
   <div class="flex flex-col gap-2">
-    <Image :src="season" alt="weather" width="250" />
-    <time class="text-sm text-right text-zinc-500 font-medium px-4" :datetime="$dayjs().utc().toString()">
-      <em>{{ dayjs().format('YYYY-MM-DD ddd') }}</em>
-    </time>
+    <Image class="rounded overflow-hidden" :src="season" alt="weather" width="300" />
+    <ClientOnly fallback-tag="div">
+      <time class="text-sm text-right text-zinc-500 font-medium px-4" :datetime="$dayjs().utc().toString()">
+        <em>{{ dayjs().format('YYYY-MM-DD ddd') }}</em>
+      </time>
+      <template #fallback>
+        <em class="text-sm text-right text-zinc-500 font-medium px-4">Loading...</em>
+      </template>
+    </ClientOnly>
   </div>
 </template>
 
